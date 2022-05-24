940e9766d914fadd80b11f64ba1e5e00335295fe
Velorexe <david.bruschke@gmail.com>
2022-05-24 18:40:44 +0200

    Fixed Character on screen placement

--- Assets/UVNF/Core/Canvas/Characters/CharacterManager.cs
+++ Assets/UVNF/Core/Canvas/Characters/CharacterManager.cs
@@ -22,6 +22,9 @@ namespace UVNF.Core.Canvas
 
         public override async UniTaskVoid Init(CancellationToken token)
         {
+            _characterCanvas = this.GetComponent<RectTransform>();
+            _canvasSize = _characterCanvas.rect.size;
+
             _cachedCharacters = UniTaskAsyncEnumerable.Create<OnScreenCharacter>(async (writer, _token) =>
             {
                 OnScreenCharacter[] characters = Resources.FindObjectsOfTypeAll<OnScreenCharacter>();
@@ -56,8 +59,8 @@ namespace UVNF.Core.Canvas
                     Debug.LogWarning("Character does not contain any poses.");
                 
                 RectTransform transform = Instantiate(character.gameObject, _characterCanvas).GetComponent<RectTransform>();
-                transform.position = new Vector2(position.x * _canvasSize.x - (_canvasSize.x / 2f), position.y * _canvasSize.y - (_canvasSize.y / 2f));
-                
+                transform.localPosition = new Vector2(position.x * _canvasSize.x - (_canvasSize.x / 2f), position.y * _canvasSize.y - (_canvasSize.y / 2f));
+               
                 Vector3 cS = transform.localScale;
                 transform.localScale = new Vector3(scale.x * cS.x, scale.y * cS.y, cS.z);
             }
